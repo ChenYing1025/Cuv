e18de53ea1c150ea2658ab36b5ea380417eb5279
Chen Ying <5565191487@qq.com>
2019-04-09 20:39:49 +0800

    a test

---
+++ hello.py
@@ -0,0 +1,16 @@
+import random
+def Cuv(i):
+    if i == 0:
+        print ("1","Cuv is a big SB.")
+    elif i < 0:
+        print ("2","Cuv is a little SB.")
+    else:
+        print("3","Cuv has a big dick.")
+#random.seed(1)
+i = int(random.choice(range(-100,100,2)))
+print(i)
+Cuv(i)
+'''import random
+def demo_random():
+    print ("1",int(random.random()*100))
+demo_random()'''
